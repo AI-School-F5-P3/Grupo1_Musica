5022e018954ba7080d7a5266016035ae1282977e
carrillo-p <alb.carrillop@gmail.com>
2024-07-08 09:43:05 +0200

    multipage gui

--- src/GUI.py
+++ src/GUI.py
@@ -1,17 +1,29 @@
 import streamlit as st
 
-st.write("Añadir alumnos a la base de datos")
+opciones = ["Registrar nuevo alumno", "Consultas BBDD"]
+seleccion = st.sidebar.selectbox("Menu", opciones)
 
-name = st.text_input("Nombre del alumno")
+if seleccion == "Registrar nuevo alumno":
+    trumpet = "\U0001f3ba"
+    sax = "\U0001f3b7"
 
-surname = st.text_input("Apellidos")
+    st.markdown(f"""<h1 style="text-align: center;"> {trumpet} {sax} Escuela Armonía{sax}{trumpet} </h1>""", unsafe_allow_html=True)
 
-age = st.number_input("Edad del alumno", value = 6, min_value = 6, max_value = 100, placeholder = "Edad..", step = 1)
+    st.markdown("""<h2 style="text-align: center;">Nuevo registro de alumno</h2>""", unsafe_allow_html=True)
 
-prefix = st.number_input("Prefijo telefono", value = 34, max_value = 99, placeholder = "Prefijo", step = 1)
+    name = st.text_input("Nombre del alumno")
 
-tfn_number = st.text_input("Número de teléfono")
+    surname = st.text_input("Apellidos")
 
-email = st.text_input("Correo electrónico")
+    age = st.number_input("Edad del alumno", value = 6, min_value = 6, max_value = 100, placeholder = "Edad..", step = 1)
 
-familiy = st.selectbox(label = "¿Tiene un familiar inscrito en nuestro centro?", options = ["Si", "No"])
+    prefix = st.number_input("Prefijo telefono", value = 34, max_value = 99, placeholder = "Prefijo", step = 1)
+
+    tfn_number = st.text_input("Número de teléfono")
+
+    email = st.text_input("Correo electrónico")
+
+    familiy = st.selectbox(label = "¿Tiene un familiar inscrito en nuestro centro?", options = ["Si", "No"])
+
+if seleccion == "Consultas BBDD":
+    st.markdown("# En construcción")
