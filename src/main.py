68a6e15949eef0049b04abe44a90a95ca415bd9a
carrillo-p <alb.carrillop@gmail.com>
2024-07-10 12:59:28 +0200

    main

--- src/main.py
+++ src/main.py
@@ -1,1 +1,32 @@
-from fastapi import FastAPI HTTPException
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Text
+from datetime import datetime
+
+app = FastAPI()
+
+posts = []
+
+class Alumno(BaseModel):
+    id: str
+    nombre: str
+    apellidos: str
+    edad: int
+    telefono: str
+    correo: str
+    familiar: bool = False
+    descuent: int
+    created_at: datetime = datetime.now()
+
+@app.get("/")
+def read_root():
+    return{"message": "this is working"}
+
+@app.get('/posts')
+def get_posts():
+    return posts
+
+@app.post('/posts')
+def save_post(post: Alumno):
+    print(post)
+    return "recieved"
