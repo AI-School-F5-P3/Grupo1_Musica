8940d15ac4230cd1f8a83763b3345bd7de350027
Mª Rosa Cuenca <mariarosacuenca@gmail.com>
2024-07-10 12:58:43 +0200

    database.py  ok

--- src/database.py
+++ src/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from ..config.config import database_url
 
-from ..config.config import database_url  # Importa la URL desde config.py
 
 SQLALCHEMY_DATABASE_URL = database_url
 
