e4f09565e9f4cf8a512aa671ce66a19fba01a5ed
Krol_Ina <carolinatomasf@gmail.com>
2024-07-10 12:30:19 +0200

    Update database.py

--- src/database.py
+++ src/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+from ..config.config import database_url
 
 
 SQLALCHEMY_DATABASE_URL = database_url
