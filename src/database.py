3462db8952213882f1bfd243cba232f47ea07ff9
Mª Rosa Cuenca <mariarosacuenca@gmail.com>
2024-07-10 10:38:23 +0200

    config

---
+++ src/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+
+SQLALCHEMY_DATABASE_URL = database_url
+
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+sessionmaker = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+Base = declarative_base
