d02ba726f7eefffbe352a9f08a16d76861ac0e49
Sweetvitta <vittoriadenovellis@gmail.com>
2024-07-11 14:07:30 +0200

    cambios

--- src/database.py
+++ src/database.py
@@ -1,14 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from config.config import database_url
+import app
 
 
+Base = declarative_base()
+engine = create_engine(database_url)
 
-SQLALCHEMY_DATABASE_URL = database_url
-
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
 sessionmaker = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-Base = declarative_base
