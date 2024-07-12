fe72e25da807700bc263356051235e7af48d73eb
Sweetvitta <vittoriadenovellis@gmail.com>
2024-07-12 10:54:24 +0200

    cambios antes del merge

--- src/database.py
+++ src/database.py
@@ -3,11 +3,13 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://usuario:contraseña@localhost/nombre_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123456@localhost:5433/armonia"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+
 sessionmaker = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-Base = declarative_base
+Base = declarative_base()
