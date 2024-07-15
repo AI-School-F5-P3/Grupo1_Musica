db08a208c15124dc5ed7f39d39d268d44182f48e
carrillo-p <alb.carrillop@gmail.com>
2024-07-15 13:36:33 +0200

    Botones de navegacion laterales fijos

--- src/GUI.py
+++ src/GUI.py
@@ -1,22 +1,16 @@
 import streamlit as st
-from GUI_screens import home_screen, screen_alumnos, screen_precios, screen_profesores
+from GUI_screens import change_screen, home_screen, screen_alumnos, screen_precios, screen_profesores, screen_nuevo_alumno
 
+# Configuración inicial de la página
 if 'screen' not in st.session_state:
     st.session_state.screen = 'home'
 
-def change_screen(new_screen):
-    st.session_state.screen = new_screen
-
 st.set_page_config(layout="wide")
-
-opciones = ["Home", "Alumnos", "Profesores", "Precios"]
-st.sidebar.header("Menu")
-seleccion = st.sidebar.selectbox("Navegación", opciones)
 
 # Custom HTML/CSS for the banner
 custom_html = """
 <div class="banner">
-    <img src="https://www.mtgpics.com/pics/art/sld/605.jpg" alt="Banner Image" height = 500 width = 1000>
+    <img src="https://www.mtgpics.com/pics/art/sld/605.jpg" alt="Banner Image" height="500" width="1000">
 </div>
 <style>
     .banner {
@@ -33,17 +27,15 @@ custom_html = """
 # Display the custom HTML
 st.components.v1.html(custom_html)
 
-# Selección menu lateral
-if seleccion == 'Home':
+# Selección del menú lateral
+st.sidebar.header("Menú de Navegación")
+if st.sidebar.button("Home"):
     change_screen('home')
-
-elif seleccion == "Alumnos":
+if st.sidebar.button("Alumnos"):
     change_screen('screen_alumnos')
-
-elif seleccion == "Profesores":
+if st.sidebar.button("Profesores"):
     change_screen('screen_profesores')
-
-elif seleccion == 'Precios':
+if st.sidebar.button("Precios"):
     change_screen('screen_precios')
 
 # Renderización de las pantallas
@@ -55,55 +47,5 @@ elif st.session_state.screen == 'screen_profesores':
     screen_profesores()
 elif st.session_state.screen == 'screen_precios':
     screen_precios()
-
-
-
-if seleccion == "Registrar nuevo alumno":
-    trumpet = "\U0001f3ba"
-    sax = "\U0001f3b7"
-
-    st.markdown(f"""<h1 style="text-align: center;"> {trumpet} {sax} Escuela Armonía{sax}{trumpet} </h1>""", unsafe_allow_html=True)
-
-    st.markdown("""<h2 style="text-align: center;">Nuevo registro de alumno</h2>""", unsafe_allow_html=True)
-
-    name = st.text_input("Nombre del alumno")
-
-    surname = st.text_input("Apellidos")
-
-    age = st.number_input("Edad del alumno", value = 6, min_value = 6, max_value = 100, placeholder = "Edad..", step = 1)
-
-    prefix = st.number_input("Prefijo telefono", value = 34, max_value = 99, placeholder = "Prefijo", step = 1)
-
-    tfn_number = st.text_input("Número de teléfono")
-
-    email = st.text_input("Correo electrónico")
-
-    familiy = st.selectbox(label = "¿Tiene un familiar inscrito en nuestro centro?", options = ["Si", "No"])
-
-    instrumento = st.selectbox("Clase", options = ["Flauta", "Piano", "Guitarra", "Saxo", "Canto"])
-
-    nivel = st.selectbox("Nivel: ", options = ["Cero", "Inicio", "Medio", "Avanzado", "No aplica"])
-
-    enviar = st.button("Registrar", type = "primary")
-
-if seleccion == "Consultas BBDD":
-    computer = "\U0001f4bb"
-    st.markdown(f"""<h1 style="text-align: center;"> {computer} Consulta base de datos {computer} </h1>""", unsafe_allow_html=True)
-
-    name = st.text_input("Nombre del alumno")
-
-    surname = st.text_input("Apellidos")
-    
-    instrumento = st.selectbox("Clase", options = ["Flauta", "Piano", "Guitarra", "Saxo", "Canto"])
-
-    nivel = st.selectbox("Nivel: ", options = ["Cero", "Inicio", "Medio", "Avanzado", "No aplica"])
-
-    enviar = st.button("Consultar", type = "primary")
-
-if seleccion == "Editar precios":
-    money = "\U0001f4b0"
-    st.markdown(f"""<h1 style="text-align: center;"> {money} Modificar precios {money} </h1>""", unsafe_allow_html=True)
-    instrumento = st.selectbox("Clase", options = ["Flauta", "Piano", "Guitarra", "Saxo", "Canto"])
-    precio = st.number_input(label = "Precio de la clase", value = 0.00 , min_value = 0.00, step = 0.01)
-    enviar = st.button("Editar", type = "primary")
-
+elif st.session_state.screen == 'screen_nuevo_alumno':
+    screen_nuevo_alumno()
--- src/GUI_screens.py
+++ src/GUI_screens.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
+def change_screen(new_screen):
+    st.session_state.screen = new_screen
+
+def change_subscreen(new_subscreen):
+    return new_subscreen
+
 def home_screen():
     trumpet = "\U0001f3ba"
     sax = "\U0001f3b7"
@@ -14,8 +20,7 @@ def screen_alumnos():
 
     st.markdown(f"""<h1 style="text-align: center;"> {trumpet} {sax} Escuela Armonía{sax}{trumpet} </h1>""", unsafe_allow_html=True)
 
-    st.markdown("""<h2 style="text-align: center;">Gestion de alumnos</h2>""", unsafe_allow_html=True)
-
+    st.markdown("""<h2 style="text-align: center;">Gestión de alumnos</h2>""", unsafe_allow_html=True)
 
     col1, col2, col3 , col4, col5 = st.columns(5)
 
@@ -28,7 +33,9 @@ def screen_alumnos():
     with col5:
         pass
     with col3:
-        st.button("Nuevo Alumno", type = "primary")
+        if st.button("Nuevo Alumno", type = "primary"):
+            change_screen('screen_nuevo_alumno')
+            st.rerun()
 
         actualizar_alumno = st.button("Actualizar datos de alumno", type = "primary")
 
@@ -62,7 +69,7 @@ def screen_profesores():
         borrar_alumno = st.button("Eliminar profesor de la base de datos", type = "primary")
 
         consultar_alumno = st.button("Consultar datos de un profesor", type = "primary")
-
+    
 def screen_precios():
     trumpet = "\U0001f3ba"
     sax = "\U0001f3b7"
@@ -89,3 +96,32 @@ def screen_precios():
         borrar_alumno = st.button("Consultar precios por pack de instrumentos", type = "primary")
 
         consultar_alumno = st.button("Consultar tipos de descuentos", type = "primary")
+
+def screen_nuevo_alumno():
+    trumpet = "\U0001f3ba"
+    sax = "\U0001f3b7"
+
+    st.markdown(f"""<h1 style="text-align: center;"> {trumpet} {sax} Escuela Armonía{sax}{trumpet} </h1>""", unsafe_allow_html=True)
+
+    st.markdown("""<h2 style="text-align: center;">Nuevo registro de alumno</h2>""", unsafe_allow_html=True)
+
+    st.text_input("Nombre del alumno")
+
+    st.text_input("Apellidos")
+
+    age = st.number_input("Edad del alumno", value = 6, min_value = 6, max_value = 100, step = 1)
+
+    prefix = st.number_input("Prefijo telefono", value = 34, max_value = 99, step = 1)
+
+    tfn_number = st.text_input("Número de teléfono")
+
+    email = st.text_input("Correo electrónico")
+
+    familiy = st.selectbox(label = "¿Tiene un familiar inscrito en nuestro centro?", options = ["Si", "No"])
+
+    instrumento = st.selectbox("Clase", options = ["Flauta", "Piano", "Guitarra", "Saxo", "Canto"])
+
+    nivel = st.selectbox("Nivel: ", options = ["Cero", "Inicio", "Medio", "Avanzado", "No aplica"])
+
+    enviar = st.button("Registrar", type = "primary")
+
