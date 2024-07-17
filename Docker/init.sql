604af01d11fb578921793a0a0da25213555919db
Mª Rosa Cuenca <mariarosacuenca@gmail.com>
2024-07-17 09:49:12 +0200

    init OK OK

--- Docker/init.sql
+++ Docker/init.sql
@@ -1,16 +1,16 @@
--- -- Establecer la codificación de caracteres a UTF8
--- \encoding UTF8
+-- Establecer la codificación de caracteres a UTF8
+\encoding UTF8
 
--- -- Conectarse a la base de datos postgres
--- \c postgres
+-- Conectarse a la base de datos postgres
+\c postgres
 
--- -- Cerrar todas las conexiones activas a la base de datos 'armonia'
--- SELECT pg_terminate_backend(pid)  -- Terminar la conexión usando el PID
--- FROM pg_stat_activity
--- WHERE datname = 'armonia' AND pid <> pg_backend_pid();  -- Excluir la conexión actual
+-- Cerrar todas las conexiones activas a la base de datos 'armonia'
+SELECT pg_terminate_backend(pid)  -- Terminar la conexión usando el PID
+FROM pg_stat_activity
+WHERE datname = 'armonia' AND pid <> pg_backend_pid();  -- Excluir la conexión actual
 
--- -- Conectarse a la base de datos 'armonia'
--- \c armonia
+-- Conectarse a la base de datos 'armonia'
+\c armonia
 
 -- Establecer la codificación de cliente a UTF8
 SET client_encoding TO 'UTF8';
@@ -393,6 +393,30 @@ BEGIN
     RETURN cantidad_clases; -- Devuelve la cantidad de clases del pack especificado
 END; $$ LANGUAGE plpgsql;
 
+-- -----------------------------  Crear la vista vista_clase_info ----------------------------- --
+
+CREATE VIEW armonia.vista_clase_info AS
+SELECT 
+    c.id AS clase_id, 
+    CONCAT(i.instrumento, ' - ', n.nivel) AS instrumento_nivel,
+    i.pack_id,
+    p.pack AS nombre_pack,
+    p.precio AS precio_clase    
+FROM 
+    armonia.clase c
+JOIN 
+    armonia.instrumento_nivel in_nivel ON c.instrumento_nivel_id = in_nivel.id
+JOIN 
+    armonia.instrumento i ON in_nivel.instrumento_id = i.id
+JOIN 
+    armonia.nivel n ON in_nivel.nivel_id = n.id
+JOIN 
+    armonia.pack p ON i.pack_id = p.id;
+
+-- Exportar los datos de la vista a un archivo CSV
+COPY (SELECT * FROM armonia.vista_clase_info) TO '/mnt/data/vista_clase_info.csv' WITH (FORMAT CSV, HEADER);
+
+SELECT * FROM armonia.vista_clase_info;
 
 -- --------------------------- funcion para modificar total_mes de alumnos -------------------------- --
 
@@ -599,75 +623,51 @@ COPY (SELECT * FROM armonia.vista_asistencia_alumnos) TO '/mnt/data/vista_asiste
 SELECT * FROM armonia.vista_asistencia_alumnos;
 
 
--- -----------------------------  Crear la vista vista_clase_info ----------------------------- --
-
-CREATE VIEW armonia.vista_clase_info AS
-SELECT 
-    c.id AS clase_id, 
-    CONCAT(i.instrumento, ' - ', n.nivel) AS instrumento_nivel,
-    i.pack_id,
-    p.pack AS nombre_pack,
-    p.precio AS precio_clase    
-FROM 
-    armonia.clase c
-JOIN 
-    armonia.instrumento_nivel in_nivel ON c.instrumento_nivel_id = in_nivel.id
-JOIN 
-    armonia.instrumento i ON in_nivel.instrumento_id = i.id
-JOIN 
-    armonia.nivel n ON in_nivel.nivel_id = n.id
-JOIN 
-    armonia.pack p ON i.pack_id = p.id;
-
--- Exportar los datos de la vista a un archivo CSV
-COPY (SELECT * FROM armonia.vista_clase_info) TO '/mnt/data/vista_clase_info.csv' WITH (FORMAT CSV, HEADER);
-
-SELECT * FROM armonia.vista_clase_info;
 
 -- ----------------------------- Creamos Roles y Usuarios -- ----------------------------- --
 
--- -- Crear un rol llamado administrator
--- DO $$
--- BEGIN
---     IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'administrator') THEN
---         CREATE ROLE administrator;
---     END IF;
--- END $$;
+-- Crear un rol llamado administrator
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'administrator') THEN
+        CREATE ROLE administrator;
+    END IF;
+END $$;
 
 
--- -- Conceder permiso de conexión a la base de datos 'armonia' para el rol administrator
--- GRANT CONNECT ON DATABASE armonia TO administrator;
+-- Conceder permiso de conexión a la base de datos 'armonia' para el rol administrator
+GRANT CONNECT ON DATABASE armonia TO administrator;
 
--- -- Conceder uso del esquema 'armonia' al rol administrator
--- GRANT USAGE ON SCHEMA armonia TO administrator;
+-- Conceder uso del esquema 'armonia' al rol administrator
+GRANT USAGE ON SCHEMA armonia TO administrator;
 
--- -- Conceder permisos de SELECT, INSERT, UPDATE y DELETE en todas las tablas del esquema 'armonia' al rol administrator
--- GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA armonia TO administrator;
+-- Conceder permisos de SELECT, INSERT, UPDATE y DELETE en todas las tablas del esquema 'armonia' al rol administrator
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA armonia TO administrator;
 
--- -- Alterar los privilegios predeterminados para que el rol administrator tenga permisos de SELECT, INSERT, UPDATE y DELETE en nuevas tablas
--- ALTER DEFAULT PRIVILEGES IN SCHEMA armonia GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO administrator;
+-- Alterar los privilegios predeterminados para que el rol administrator tenga permisos de SELECT, INSERT, UPDATE y DELETE en nuevas tablas
+ALTER DEFAULT PRIVILEGES IN SCHEMA armonia GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO administrator;
 
--- -- Conceder uso de todas las secuencias en el esquema 'armonia' al rol administrator
--- GRANT USAGE ON ALL SEQUENCES IN SCHEMA armonia TO administrator; -- Las secuencias en bases de datos son objetos que generan números únicos y secuenciales. Se utilizan comúnmente para crear identificadores únicos para filas en tablas, especialmente para columnas de tipo clave primaria.
+-- Conceder uso de todas las secuencias en el esquema 'armonia' al rol administrator
+GRANT USAGE ON ALL SEQUENCES IN SCHEMA armonia TO administrator; -- Las secuencias en bases de datos son objetos que generan números únicos y secuenciales. Se utilizan comúnmente para crear identificadores únicos para filas en tablas, especialmente para columnas de tipo clave primaria.
 
--- -- Alterar los privilegios predeterminados para que el rol administrator tenga permisos de USAGE en nuevas secuencias
--- ALTER DEFAULT PRIVILEGES IN SCHEMA armonia GRANT USAGE ON SEQUENCES TO administrator;
+-- Alterar los privilegios predeterminados para que el rol administrator tenga permisos de USAGE en nuevas secuencias
+ALTER DEFAULT PRIVILEGES IN SCHEMA armonia GRANT USAGE ON SEQUENCES TO administrator;
 
--- -- Crear un usuario llamado admin_user con una contraseña
--- CREATE USER admin_user WITH LOGIN PASSWORD '1234';
+-- Crear un usuario llamado admin_user con una contraseña
+CREATE USER admin_user WITH LOGIN PASSWORD '1234';
 
--- -- Conceder el rol administrator al usuario admin_user
--- GRANT administrator TO admin_user;
+-- Conceder el rol administrator al usuario admin_user
+GRANT administrator TO admin_user;
 
--- -- Consultar los roles existentes y sus miembros, excluyendo roles del sistema
--- SELECT
---     r.rolname,  -- Nombre del rol
---     ARRAY(SELECT b.rolname  -- Array de nombres de roles a los que pertenece
---           FROM pg_catalog.pg_auth_members m
---           JOIN pg_catalog.pg_roles b ON (m.roleid = b.oid)
---           WHERE m.member = r.oid) as memberof
--- FROM pg_catalog.pg_roles r
--- WHERE r.rolname NOT IN ('pg_signal_backend','rds_iam',
---                          'rds_replication','rds_superuser',
---                          'rdsadmin','rdsrepladmin')  -- Excluir roles del sistema
--- ORDER BY 1;  -- Ordenar por el nombre del rol
+-- Consultar los roles existentes y sus miembros, excluyendo roles del sistema
+SELECT
+    r.rolname,  -- Nombre del rol
+    ARRAY(SELECT b.rolname  -- Array de nombres de roles a los que pertenece
+          FROM pg_catalog.pg_auth_members m
+          JOIN pg_catalog.pg_roles b ON (m.roleid = b.oid)
+          WHERE m.member = r.oid) as memberof
+FROM pg_catalog.pg_roles r
+WHERE r.rolname NOT IN ('pg_signal_backend','rds_iam',
+                         'rds_replication','rds_superuser',
+                         'rdsadmin','rdsrepladmin')  -- Excluir roles del sistema
+ORDER BY 1;  -- Ordenar por el nombre del rol
